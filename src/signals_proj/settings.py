8de6012d1cfe2889817ad98ebf1670a427b89903
henryfrstr <63148122+henryfrstr@users.noreply.github.com>
2020-12-22 11:59:32 +0300

    added m2m signals

--- src/signals_proj/settings.py
+++ src/signals_proj/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'buyers.apps.BuyersConfig',
     'cars.apps.CarsConfig',
-    'orders',
+    'orders.apps.OrdersConfig',
     'sales',
 ]
 
